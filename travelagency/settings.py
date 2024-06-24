bb11dfe884ba3da532f2f9cd2319447640751d7a
Farhad Firozitabar <111887328+Farhad-Firozitabar@users.noreply.github.com>
2024-06-24 07:33:18 +0330

    Update settings.py

--- travelagency/settings.py
+++ travelagency/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dyz1(x9_73=-0bi3iqr_-k%2y^@x#+1_7kngbg&#zc@$1+uh=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['travel-agancey.onrender.com', 'localhost']
 
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
